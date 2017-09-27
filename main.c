6521dcad5bd8922c4150d2384477552e37a57b2d
roopansh <roopansh.bansal@gmail.com>
2017-09-27 18:17:59 +0530

    Added ls and rm back and some bug fixes
    fixed rmexcept delelting '.' and '..'

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
+#define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>		
+#include <stdbool.h>
 #include <string.h>
 #include <malloc.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <dirent.h>
-
+#include <ftw.h>
 
 /*
 	Function Declarations for builtin shell commands:
@@ -18,6 +21,7 @@ int sh_help(char **args);
 int sh_history(char** args);
 int sh_issue(char **args);
 int sh_rm(char **args);
+int sh_ls(char **args);
 int sh_rmexcept(char **args);
 int sh_exit(char **args);
 
@@ -39,6 +43,7 @@ char *builtin_str[] = {
 	"cd",
 	"history",
 	"issue",
+	"ls",
 	"rm",
 	"rmexcept",
 	"exit"
@@ -49,6 +54,7 @@ int (*builtin_func[]) (char **) = {
 	&sh_cd,
 	&sh_history,
 	&sh_issue,
+	&sh_ls,
 	&sh_rm,
 	&sh_rmexcept,
 	&sh_exit
@@ -65,22 +71,19 @@ int sh_num_builtins() {
 /*
 	Builtin function implementations.
 */
-int sh_help(char **args)
-{
+int sh_help(char **args) {
 	int i;
-	printf("Type program names and arguments, and hit enter.\n");
+	printf("\nType program names and arguments, and hit enter.\n");
 	printf("The following are built in commands:\n");
 
 	for (i = 0; i < sh_num_builtins(); i++) {
 		printf("  %s\n", builtin_str[i]);
 	}
-
-	// printf("Use the man command for information on other programs.\n");
+	printf("\n");
 	return 1;
 }
 
-int sh_cd(char **args)
-{
+int sh_cd(char **args) {
 	if (args[1] == NULL) {
 		fprintf(stderr, "sh: expected argument to \"cd\"\n");
 	} else {
@@ -91,27 +94,26 @@ int sh_cd(char **args)
 	return 1;
 }
 
-int sh_history(char**args)
-{
-	if (args[1] == NULL) {
-		fprintf(stderr, "sh: expected argument to \"history\"\n");
+int sh_history(char**args) {
+	int j,i;
+	if (args[1] == NULL) {			// print complete history
+		i = 0;
 	} else {
-		int j,i = commandCount - atoi(args[1]); //printing most recent args[1] no. of commands
+		i = commandCount - atoi(args[1]); //printing most recent args[1] no. of commands
 		if(i<0)i=0;
-		for(j=i;j < commandCount;j++)
-			printf("%d\t%s\n", j+1, commands[j]);
 	}
+	for(j = i; j < commandCount; j++)
+		printf("\t%d\t%s\n", j+1, commands[j]);
 	return 1;
 }
 
-int sh_issue(char **args)
-{
+int sh_issue(char **args) {
 	if (args[1] == NULL) {
 		fprintf(stderr, "sh: expected argument to \"issue\"\n");
 	} else {
 		int n = atoi(args[1]);
 		if(n <= 0 || n > commandCount){
-			fprintf(stderr, "sh: invalid argument to \"issue\"\n. Argument out of range\n");
+			fprintf(stderr, "sh: invalid argument to \"issue\". Argument out of range\n");
 			return 1;
 		}
 
@@ -125,72 +127,136 @@ int sh_issue(char **args)
 	return 1;
 }
 
-int sh_rm(char **args)
-{
-	if (args[1] == NULL) {
-		fprintf(stderr, "sh: expected file name to \"rm\"\n");
-	} else {
-		char filename[2048];
-		getcwd(filename, sizeof(filename));
-		strcat(filename, "/"); 
-		strcat(filename, args[1]); 
-		if (unlink(filename) != 0) {
-			perror("sh");
+int sh_ls(char **args) {
+	struct dirent **namelist;
+	int n = scandir(".", &namelist, NULL, alphasort);
+	if(n < 0)
+	{
+		perror("sh");
+	}
+	else
+	{
+		while (n--)
+		{
+			printf("%s\n", namelist[n]->d_name);
+			free(namelist[n]);
 		}
+		free(namelist);
 	}
 	return 1;
 }
 
-int sh_rmexcept(char **args)
-{
+int unlink_cb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {		
+	int rv = remove(fpath);
+
+	if (rv)
+		perror(fpath);
+		
+	return rv;
+}
+		
+int unlink_cb_verb(const char *fpath, const struct stat *sb, int typeflag, struct FTW *ftwbuf) {
+	int rv = remove(fpath);
+	if (rv)
+		perror(fpath);
+	else	printf("%s\n", fpath);
+	return rv;
+ }
+
+int sh_rm(char **args) {
+	/* Check for options used. */
+	int argc = 1;
+	bool RECURSIVE_FLAG = false,
+		VERBOSE_FLAG = false,
+		FORCE_FLAG = false;
+	char filename[2048] = "";
+
+	while(args[argc] != NULL)
+	{
+		if (strcmp(args[argc], "-r") == 0) {
+			RECURSIVE_FLAG = true;
+		} else if (strcmp(args[argc], "-f") == 0) {
+			FORCE_FLAG = true;
+		} else if (strcmp(args[argc], "-v") == 0) {
+			VERBOSE_FLAG = true;
+		} else {
+			// absolute address
+			if (args[argc][0] != '/') {
+				// relative address
+				getcwd(filename, sizeof(filename));
+				strcat(filename, "/");
+			}
+			strcat(filename, args[argc]);
+		}
+		argc++;
+	}
+
+	if (filename == NULL) {
+		fprintf(stderr, "sh: expected a file name to \"rm\"\n");
+	} else if (!RECURSIVE_FLAG) {						// deleting a single file
+		if (unlink(filename) != 0) {
+			perror("sh");
+			return 1;
+		}
+		if (VERBOSE_FLAG)	printf("%s\n", filename);
+	} else {											// deleting a directory
+		if(VERBOSE_FLAG)	nftw(filename, unlink_cb_verb, 64, FTW_DEPTH | FTW_PHYS);
+		else	nftw(filename, unlink_cb, 64, FTW_DEPTH | FTW_PHYS);
+	}
+	return 1;
+}
+
+int sh_rmexcept(char **args) {
 	//int size = sizeof(args)/sizeof(args[0]);
-	int j,i = 0,flag=0;
-	while(args[i]!=NULL)i++;
+	int j, i = 0, flag = 0;
+
+	while(args[i] != NULL)	i++;
 
 	DIR *d;
-    struct dirent *dir;
-    d = opendir(".");
-    if (d)
-    {
-        while ((dir = readdir(d)) != NULL)
-        {
-        	for(j=1;j<i;j++)
-        	{
-        		if(strcmp(args[j], dir->d_name) == 0)
-        		{
-        			flag=0;
-        			break;
-        		}
-        		else
-        			flag=1;
-        	}
-        	
-        	if(flag==1)
-        	{
-        		char filename[2048];
+	struct dirent *dir;
+	
+	d = opendir(".");
+	if (d) {
+		while ((dir = readdir(d)) != NULL) {
+
+			if (strcmp(dir->d_name, ".") == 0 || strcmp(dir->d_name, "..") == 0) {
+				continue;
+			}
+
+			for(j = 1; j < i; j++) {
+				if( strcmp(args[j], dir->d_name) == 0 ) {
+					flag = 0;
+					break;
+				} else {
+					flag = 1;
+				}
+			}
+			
+			if (flag == 1) {
+				char filename[2048] = "";
 				getcwd(filename, sizeof(filename));
 				strcat(filename, "/"); 
 				strcat(filename, dir->d_name); 
 				if (unlink(filename) != 0) {
 					perror("sh");
 				}	
-        	}
-        }
-        closedir(d);
-    }
+				printf("%s\n", filename);
+			}
+		}
+
+		closedir(d);
+	}
 	return 1;
 }
 
-int sh_exit(char **args)
-{
+int sh_exit(char **args) {
 	return 0;
 }
 
 /*
 	For executing programs
 */
-int sh_launch(char **args)
-{
+int sh_launch(char **args) {
 	pid_t pid, wpid;
 	int status;
 
@@ -214,8 +280,7 @@ int sh_launch(char **args)
 	return 1;
 }
 
-int sh_execute(char **args)
-{
+int sh_execute(char **args) {
 	int i;
 
 	if (args[0] == NULL) {
@@ -223,12 +288,15 @@ int sh_execute(char **args)
 		return 1;
 	}
 
+	if (strcmp(args[0], "cd") == 0) {
+		return (*builtin_func[1])(args);
+	} else if (strcmp(args[0], "rm") == 0) {
+		return (*builtin_func[5])(args);	
+	} else if (strcmp(args[0], "rmexcept") == 0) {
+		return (*builtin_func[6])(args);
+	}
 	for (i = 0; i < sh_num_builtins(); i++) {
-		if(strcmp(args[0], "cd") == 0)
-		{
-			return (*builtin_func[1])(args);
-		}
-		else if (strcmp(args[0], builtin_str[i]) == 0) {
+		if (strcmp(args[0], builtin_str[i]) == 0) {
 
 			pid_t pid, wpid;
 			int status;
@@ -257,8 +325,7 @@ int sh_execute(char **args)
 */
 #define sh_TOK_BUFSIZE 64
 #define sh_TOK_DELIM " \t\r\n\a"
-char **sh_split_line(char *line)
-{
+char **sh_split_line(char *line) {
 	int bufsize = sh_TOK_BUFSIZE, position = 0;
 	char **tokens = malloc(bufsize * sizeof(char*));
 	char *token;
@@ -289,8 +356,7 @@ char **sh_split_line(char *line)
 }
 
 #define sh_RL_BUFSIZE 1024
-char *sh_read_line(void)
-{
+char *sh_read_line(void) {
 	int bufsize = sh_RL_BUFSIZE;
 	int position = 0;
 	char *buffer = malloc(sizeof(char) * bufsize);
@@ -327,8 +393,7 @@ char *sh_read_line(void)
 }
 
 #define sh_HIST_BUFFER 10
-void sh_loop(void)
-{
+void sh_loop(void) {
 	char *line;
 	char **args;
 	int status;
